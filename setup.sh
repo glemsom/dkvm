8a7670feed8a0e7693842af4bcbfd9486a304ca6
Glenn Sommer <glemsom@gmail.com>
2025-03-12 16:35:24 +0100

    Upgrade to Alpine 3.21.3

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 version=0.5.8
 disksize=2048 #Disk size in MB
 alpineVersion=3.21
-alpineVersionMinor=2
+alpineVersionMinor=3
 alpineISO=alpine-standard-${alpineVersion}.${alpineVersionMinor}-x86_64.iso
 ovmf_code=OVMF_CODE.fd
 ovmf_vars=OVMF_VARS.fd
