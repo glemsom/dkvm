e8e7f60cba9ddc8e2f9226c912e3997593bfdfcd
Glenn Sommer <glemsom@gmail.com>
2026-05-14 18:12:55 +0000

    chore: release v0.7.20 - fix qemu-system-x86_64 repo, add .lean-ctx to gitignore

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -86,9 +86,14 @@ fi
 echo "Enable extra repositories"
 sed -i '/^#.*v3.*community/s/^#/@community /' /etc/apk/repositories
 
+# Add DKVM-QEMU repository
+echo "Adding DKVM-QEMU repository"
+wget -O /etc/apk/keys/signkey.rsa.pub https://raw.githubusercontent.com/glemsom/dkvm-qemu/main/keys/signkey.rsa.pub
+echo "https://glemsom.github.io/dkvm-qemu/" >>/etc/apk/repositories
+
 apk update
 apk upgrade
-apk add ca-certificates wget util-linux bridge bridge-utils amd-ucode intel-ucode qemu-img@community qemu-hw-usb-host@community qemu-system-x86_64@community ovmf@community qemu-hw-display-virtio-vga@community swtpm@community bash dialog bc nettle jq vim lvm2 lvm2-dmeventd e2fsprogs pciutils irqbalance hwloc-tools || err "Cannot install packages"
+apk add ca-certificates wget util-linux bridge bridge-utils amd-ucode intel-ucode qemu-img@community qemu-hw-usb-host@community qemu-system-x86_64@dkvm ovmf@community qemu-hw-display-virtio-vga@community swtpm@community bash dialog bc nettle jq vim lvm2 lvm2-dmeventd e2fsprogs pciutils irqbalance hwloc-tools || err "Cannot install packages"
 
 # ╔═══════════════════════════════════════════════════════════════════════════════════╗
 # ║ Configure GRUB with IOMMU and VFIO kernel parameters for DKVM
